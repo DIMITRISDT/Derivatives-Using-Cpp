a611e3ba0491f2c7902325f4c94ab4331d6c82da
gingerducky <225703486+gingerducky@users.noreply.github.com>
2026-05-14 20:32:13 +0300

    ..

--- include/Stack.hpp
+++ include/Stack.hpp
@@ -1,6 +1,9 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include <cassert>
+#include <iostream>
+
 template<typename T>
 struct Node {
     Node<T>* previous;
@@ -12,12 +15,62 @@ class Stack {
     Node<T>* current;
     int size;
 public:
-    Stack();
-    virtual ~Stack();
-    bool push(T val);
-    T pop();
-    int getSize() const;
-    const T& top() const;
+    Stack()
+        :size(0), current(nullptr)
+    {}
+    virtual ~Stack() {
+        while (current)
+        {
+            Node<T>* tmp = current;
+            current = current->previous;
+            delete tmp;
+        }
+    }
+    bool push(T value)
+    {
+        Node<T>* tmp = new Node<T>;
+        tmp->val = value;
+        tmp->previous = current;
+        current = tmp;
+        ++size;
+        return true;
+    }
+    T pop()
+    {
+        if (size == 0)
+        {
+            std::cerr << "Can't use pop() method on empty Stack!" << std::endl;
+            assert(false);
+        }
+
+        Node<T>* tmp = current;
+        T tmpVal = tmp->val;
+        if (size == 1)
+            current = nullptr;
+        else if (size > 1) 
+        {
+            current = current->previous;
+        }
+
+        --size;
+        delete tmp;
+        return tmpVal;
+    }
+    int getSize() const
+    {
+        return size;
+    }
+    const T& top() const
+    {
+        if (size == 0)
+        {
+            std::cerr << "Stack is empty!" << std::endl;
+            assert(false);
+        }
+        return current->val;
+    }
+    Stack(const Stack&) = delete;
+    Stack& operator=(const Stack&) = delete;
 };
 
 #endif
--- include/derivatives.hpp
+++ include/derivatives.hpp
@@ -3,10 +3,24 @@
 #include <iostream>
 #include <string>
 
-class DerivativeSolver {
+class DerivativeStringSolver {
     
 public:
+    std::string constant(std::string c);
+    int constant(int c);
 
+    std::string linear(std::string c);
+    int linear(int c);
+
+    std::string numberPower(char base, std::string power);
+    std::string numberPower(char base, int power);
+
+    std::string eBasePower(std::string power);
+    std::string eBasePower(std::string power, std::string multi) // continue from here
+
+    std::string exponential(std::string input);
+    std::string naturalLog(std::string input);
+    std::string logarithm(std::string input);
 }
 
 
