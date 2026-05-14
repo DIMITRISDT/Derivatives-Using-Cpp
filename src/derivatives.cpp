b9d981516bb5655c1d886a75c6162de95db0bde9
DIMITRISDT <dimitristaxmapro@gmail.com>
2026-05-14 22:46:49 +0300

    term saving (done)

--- src/derivatives.cpp
+++ src/derivatives.cpp
@@ -1,48 +1,48 @@
-// #include "derivatives.hpp"
+#include "derivatives.hpp"
 
-// using std::string;
+using std::string;
 
-// string DerivativeStringSolver::constant(string c)
-// {
-//     string tmp("0");
-//     return tmp;
-// }
+string DerivativeStringSolver::constant(string c)
+{
+    string tmp("0");
+    return tmp;
+}
 
-// int DerivativeStringSolver::constant(int c)
-// {
-//     return 0;
-// }
+int DerivativeStringSolver::constant(int c)
+{
+    return 0;
+}
 
-// string DerivativeStringSolver::linear(string c)
-// {
-//     string tmp("1");
-//     return tmp;
-// }
+string DerivativeStringSolver::linear(string c)
+{
+    string tmp("1");
+    return tmp;
+}
 
-// int DerivativeStringSolver::linear(int c)
-// {
-//     return 1;
-// }
+int DerivativeStringSolver::linear(int c)
+{
+    return 1;
+}
 
-// string DerivativeStringSolver::numberPower(char base, string power)
-// {
-//     int powerNum = stoi(power);
-//     string newPower = to_string(--powerNum);
-//     string result;
-//     result = power + base + "^" + newPower;
-//     return result;
-// }
+string DerivativeStringSolver::numberPower(char base, string power)
+{
+    int powerNum = stoi(power);
+    string newPower = to_string(--powerNum);
+    string result;
+    result = power + base + "^" + newPower;
+    return result;
+}
 
-// string DerivativeStringSolver::numberPower(char base, int power)
-// {
-//     string result;
-//     result = to_string(power) + base + "^" + to_string(--power);
-//     return result;
-// }
+string DerivativeStringSolver::numberPower(char base, int power)
+{
+    string result;
+    result = to_string(power) + base + "^" + to_string(--power);
+    return result;
+}
 
-// string DerivativeStringSolver::eBasePower(string power)
-// {
-//     string result;
-//     result = "e^x";
-//     return result;
-// }
+string DerivativeStringSolver::eBasePower(string power)
+{
+    string result;
+    result = "e^x";
+    return result;
+}
